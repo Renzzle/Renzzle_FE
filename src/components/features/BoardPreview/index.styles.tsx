dfb44904f28d11a7700fca6a32c1e2b81d5aaf9c
cyLee1111 <flora021111@gmail.com>
2024-11-05 19:57:23 +0900

    feat: add BoardPrevie w component

---
+++ src/components/features/BoardPreview/index.styles.tsx
@@ -0,0 +1,10 @@
+import { View } from 'react-native';
+import styled from 'styled-components';
+
+export const Preview = styled(View)`
+  width: 90px;
+  height: 90px;
+  border-radius: 5px;
+`;
+
+export default Preview;
---
+++ src/components/features/BoardPreview/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Preview from './index.styles';
+
+const BoardPreview = () => {
+  return (
+    <Preview />
+  );
+};
+
+export default BoardPreview;
