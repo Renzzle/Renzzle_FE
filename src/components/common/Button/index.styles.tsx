df94e0a65e78bc6c35a14deb5be3b7f37fc17adb
cyLee1111 <flora021111@gmail.com>
2024-11-07 17:59:34 +0900

    feat: add Button component

---
+++ src/components/common/Button/index.styles.tsx
@@ -0,0 +1,24 @@
+import { TouchableOpacity } from 'react-native';
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+export const StyledButton = styled(TouchableOpacity)`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  padding: 17px 0 16px 0;
+  border-radius: 6px;
+  border-width: 0;
+`;
+
+export const StyledPrimaryButton = styled(StyledButton)<{ disabled: boolean }>`
+  background-color: ${({ disabled }) =>
+    disabled ? theme.color['gray/gray100'] : theme.color['sub_color/green/bg']
+  };
+`;
+
+export const StyledSecondaryButton = styled(StyledButton)<{ disabled: boolean }>`
+  background-color: ${({ disabled }) =>
+    disabled ? theme.color['gray/gray100'] : theme.color['sub_color/green/s']
+  };
+`;
