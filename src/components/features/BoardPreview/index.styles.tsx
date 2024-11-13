637b0bd4426802b10abb38707748424cd5e159a2
cyLee1111 <flora021111@gmail.com>
2024-11-13 16:55:54 +0900

    feat: add lock icon at locked preview

--- src/components/features/BoardPreview/index.styles.tsx
+++ src/components/features/BoardPreview/index.styles.tsx
@@ -6,7 +6,9 @@ export const Preview = styled(View)<{ isLocked: boolean }>`
   width: 90px;
   height: 90px;
   border-radius: 5px;
-  background-color: ${theme.color['sub_color/beige/c']};
+  align-items: center;
+  justify-content: center;
+  
   background-color: ${({ isLocked }) =>
     isLocked ? theme.color['sub_color/beige/s'] : theme.color['sub_color/beige/c']
   };
