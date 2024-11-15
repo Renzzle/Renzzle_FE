e7673bc481142ee8d51210609fb7291963577543
cyLee1111 <flora021111@gmail.com>
2024-11-15 16:11:27 +0900

    feat: fetch CommunityPuzzleList api

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { Difficulty, WinColor } from '../components/features/Puzzle/index.types';
+
 export const BOARD_SIZE = 15;
 
 export const convertToReverseNumber = (num: number): number => {
@@ -19,4 +21,28 @@ export const convertToLowercaseAlphabet = (num: number): string => {
     throw new Error('Input number must be between 0 and 14');
   }
   return String.fromCharCode(97 + num);
+};
+
+export const toDifficultyEnum = (key: string): Difficulty | undefined => {
+  switch (key) {
+    case 'HIGH':
+      return Difficulty.HIGH;
+    case 'MIDDLE':
+      return Difficulty.MIDDLE;
+    case 'LOW':
+      return Difficulty.LOW;
+    default:
+      return undefined;
+  }
+};
+
+export const toWinColorEnum = (key: string): WinColor | undefined => {
+  switch (key) {
+    case 'BLACK':
+      return WinColor.BLACK;
+    case 'WHITE':
+      return WinColor.WHITE;
+    default:
+      return undefined;
+  }
 };
