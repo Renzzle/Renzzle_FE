f741063282f18e23b39f3d11e5e164805f900bba
cyLee1111 <flora021111@gmail.com>
2024-11-23 17:18:41 +0900

    refactor: change API calls from async/await fetch to axios

--- src/apis/auth.ts
+++ src/apis/auth.ts
@@ -1,48 +1,21 @@
-import { HTTP_HEADERS, HTTP_HEADERS_VALUES, HTTP_METHODS } from './constants';
+import apiClient from './interceptor';
 
 export const updateEmailAuthCode = async (email: string) => {
   try {
-    const response = await fetch(`${process.env.API_URL}/api/auth/email` , {
-      method: HTTP_METHODS.POST,
-      headers: {
-        [HTTP_HEADERS.CONTENT_TYPE]: HTTP_HEADERS_VALUES.JSON,
-      },
-      body: JSON.stringify({ email }),
-    });
+    const response = await apiClient.post('/api/auth/email', { email });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
+    return response.data;
   } catch (error) {
-    console.error('Error fetching data: ', error);
     throw error;
   }
 };
 
 export const getAuth = async (email: string, password: string) => {
   try {
-    const response = await fetch(`${process.env.API_URL}/api/auth/login` , {
-      method: HTTP_METHODS.POST,
-      headers: {
-        [HTTP_HEADERS.CONTENT_TYPE]: HTTP_HEADERS_VALUES.JSON,
-      },
-      body: JSON.stringify({ email, password }),
-    });
+    const response = await apiClient.post('/api/auth/login', { email, password });
 
-    if (!response.ok) {
-      const errorData = await response.json();
-      const errorMessage = errorData?.errorResponse?.message || 'An unknown error occurred.';
-      throw new Error(`HTTP error! status: ${response.status} / message: ${errorMessage}`);
-    }
-
-    const data = await response.json();
-    console.log('로그인데이터:', data);
-    return data;
+    return response.data;
   } catch (error) {
-    console.error('Error fetching data: ', error);
     throw error;
   }
 };
