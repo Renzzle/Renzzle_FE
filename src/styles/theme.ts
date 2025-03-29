26dbd5417ac657ab411a5c9eb9fcbeee27341404
cyLee1111 <flora021111@gmail.com>
2025-03-29 19:11:38 +0900

    refactor: update theme colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,17 +1,22 @@
 export type ColorType =
   | 'gray/white'
   | 'gray/grayBG'
+  | 'gray/grayBGDim'
   | 'gray/gray50'
   | 'gray/gray100'
   | 'gray/gray150'
   | 'gray/gray200'
+  | 'gray/gray300'
   | 'gray/gray400'
   | 'gray/gray500'
   | 'gray/gray600'
   | 'gray/gray800'
   | 'gray/gray900'
   | 'gray/black'
-  | 'main_color/green'
+  | 'main_color/blue_p'
+  | 'main_color/blue_s'
+  | 'main_color/yellow_p'
+  | 'main_color/yellow_s'
   | 'sub_color/red/p'
   | 'sub_color/red/s'
   | 'sub_color/red/c'
@@ -20,16 +25,17 @@ export type ColorType =
   | 'sub_color/orange/s'
   | 'sub_color/orange/c'
   | 'sub_color/orange/bg'
+  | 'sub_color/yellow/p'
+  | 'sub_color/yellow/s'
+  | 'sub_color/yellow/c'
+  | 'sub_color/yellow/bg'
   | 'sub_color/beige/p'
-  | 'sub_color/beige/s'
   | 'sub_color/beige/c'
-  | 'sub_color/beige/h'
   | 'sub_color/beige/d'
   | 'sub_color/beige/bg'
   | 'sub_color/green/p'
   | 'sub_color/green/s'
   | 'sub_color/green/c'
-  | 'sub_color/green/h'
   | 'sub_color/green/bg'
   | 'sub_color/blue/p'
   | 'sub_color/blue/s'
@@ -55,36 +61,42 @@ const theme = {
   color: {
     'gray/white': '#FFFFFF',
     'gray/grayBG': '#FBFCFD',
+    'gray/grayBGDim': '#F8F9FA',
     'gray/gray50': '#F2F5F8',
     'gray/gray100': '#EBEEF2',
     'gray/gray150': '#DCE2E7',
     'gray/gray200': '#CED3DA',
+    'gray/gray300': '#B9C2CB',
     'gray/gray400': '#A3ABB3',
     'gray/gray500': '#767D86',
     'gray/gray600': '#4B535A',
     'gray/gray800': '#2C2F34',
     'gray/gray900': '#232629',
     'gray/black': '#111111',
-    'main_color/green': '#649B92',
-    'sub_color/red/p': '#F9557C',
+    'main_color/blue_p': '#4340DB',
+    'main_color/blue_s': '#302DAC',
+    'main_color/yellow_p': '#FFA722',
+    'main_color/yellow_s': '#FF7A00',
+    'sub_color/red/p': '#FA5D68',
     'sub_color/red/s': '#FFABC1',
     'sub_color/red/c': '#FEE0E8',
-    'sub_color/red/bg': '#FEF4F7',
+    'sub_color/red/bg': '#FFEFEF',
     'sub_color/orange/p': '#F0836C',
-    'sub_color/orange/s': '#FFBCAE',
+    'sub_color/orange/s': '#FFBBAD',
     'sub_color/orange/c': '#FFDBD3',
-    'sub_color/orange/bg': '#FFF1EE',
-    'sub_color/beige/p': '#F2CA94',
-    'sub_color/beige/s': '#BBB2A6',
-    'sub_color/beige/c': '#F8EBDB',
-    'sub_color/beige/h': '#F2DEC3',
+    'sub_color/orange/bg': '#FFF1E7',
+    'sub_color/yellow/p': '#FF9900',
+    'sub_color/yellow/s': '#FFD88C',
+    'sub_color/yellow/c': '#FFE9BF',
+    'sub_color/yellow/bg': '#FFF7E6',
+    'sub_color/beige/p': '#F4BE74',
+    'sub_color/beige/c': '#FBEFE1',
     'sub_color/beige/d': '#988A77',
-    'sub_color/beige/bg': '#F4F0EB',
-    'sub_color/green/p': '#658780',
+    'sub_color/beige/bg': '#FFF6EA',
+    'sub_color/green/p': '#569584',
     'sub_color/green/s': '#81B8A2',
     'sub_color/green/c': '#ADCEC1',
-    'sub_color/green/h': '#D2E7DF',
-    'sub_color/green/bg': '#E8F5F0',
+    'sub_color/green/bg': '#E9F9F4',
     'sub_color/blue/p': '#52A5E1',
     'sub_color/blue/s': '#AFD4FB',
     'sub_color/blue/c': '#D2E7FD',
@@ -92,7 +104,7 @@ const theme = {
     'sub_color/indigo/p': '#7084EC',
     'sub_color/indigo/s': '#CAC8FA',
     'sub_color/indigo/c': '#E3E3FF',
-    'sub_color/indigo/bg': '#F0F0FF',
+    'sub_color/indigo/bg': '#E9ECFD',
     'sub_color/purple/p': '#8B5DC5',
     'sub_color/purple/s': '#C6B0FF',
     'sub_color/purple/c': '#E5DCFD',
