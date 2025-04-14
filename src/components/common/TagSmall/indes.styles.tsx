5746ef1885f7a7259727929303cdecc77b229623
cyLee1111 <flora021111@gmail.com>
2025-04-14 17:58:43 +0900

    remove: TagSmall

--- src/components/common/TagSmall/indes.styles.tsx
+++
@@ -1,9 +0,0 @@
-import { View } from 'react-native';
-import styled from 'styled-components';
-import theme from '../../../styles/theme';
-
-export const TagContainer = styled(View)`
-  background-color: ${theme.color['gray/gray50']};
-  padding: 2px 3px 1px;
-  border-radius: 2px;
-`;
