51acdf979168bd7f415531b8f83ed310433f2ad4
cyLee1111 <flora021111@gmail.com>
2025-04-22 15:32:50 +0900

    refactor: add index.ts to components/common

---
+++ src/components/common/index.ts
@@ -0,0 +1,8 @@
+export { default as BottomButtonBar } from './BottomButtonBar';
+export { default as CustomButton } from './CustomButton';
+export { default as CustomModal } from './CustomModal';
+export { default as CustomTag } from './CustomTag';
+export { default as CustomText } from './CustomText';
+export { default as CustomTextInput } from './CustomTextInput';
+export { default as DividerThin } from './DividerThin';
+export { default as Icon } from './Icon';
