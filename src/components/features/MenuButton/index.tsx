e9445001db864248ac55048fc9b7ca457e09d932
cyLee1111 <flora021111@gmail.com>
2025-04-30 11:04:23 +0900

    feat: add MenuButton component

---
+++ src/components/features/MenuButton/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { ButtonWrapper } from './index.styles';
+import { Icon } from '../../common';
+import { menuThemeMap, MenuType } from '../../types/MenuThemeMap';
+import theme from '../../../styles/theme';
+
+interface MenuButtonProps {
+  type: MenuType;
+  size?: number;
+}
+
+const MenuButton = ({ type, size = 40 }: MenuButtonProps) => {
+  const menuTheme = menuThemeMap[type];
+
+  return (
+    <ButtonWrapper background={theme.color[menuTheme.background]} size={size}>
+      <Icon name={menuTheme.iconName} color={menuTheme.iconColor} size={size * 0.625} />
+    </ButtonWrapper>
+  );
+};
+
+export default MenuButton;
