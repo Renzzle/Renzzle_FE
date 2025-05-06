0ddf4cb457c9f9bdb83059a436007d7d913e7913
cyLee1111 <flora021111@gmail.com>
2025-05-06 19:26:35 +0900

    feat: add PackCard component

---
+++ src/components/features/PackCard/index.styles.tsx
@@ -0,0 +1,35 @@
+import { TouchableOpacity, View } from 'react-native';
+import styled from 'styled-components';
+import theme from '../../../styles/theme';
+
+export const CardContainer = styled(TouchableOpacity)<{ variant: string }>`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: ${({ variant }) =>
+    variant === 'default' ? theme.color['gray/white'] : 'transparent'};
+  padding: 20px 30px;
+  border-radius: 13px;
+`;
+
+export const AuthorWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  gap: 3px;
+  margin: 5px 0 3px;
+`;
+
+export const CardRightWrapper = styled(View)`
+  display: flex;
+  align-items: center;
+  min-width: 53px;
+  gap: 1px;
+`;
+
+export const PriceWrapper = styled(View)`
+  display: flex;
+  flex-direction: row;
+  gap: 2px;
+  align-items: center;
+`;
