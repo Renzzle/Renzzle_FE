1e3f525044350c5c31b9e1d95ac27c68d07fccc4
cyLee1111 <flora021111@gmail.com>
2025-05-19 21:16:52 +0900

    refactor: add color variant to CustomTag component

--- src/components/common/CustomTag/index.styles.tsx
+++ src/components/common/CustomTag/index.styles.tsx
@@ -1,9 +1,9 @@
 import { View } from 'react-native';
 import styled from 'styled-components';
-import theme from '../../../styles/theme';
+import theme, { ColorType } from '../../../styles/theme';
 
-export const TagContainer = styled(View)`
-  background-color: ${theme.color['sub_color/yellow/bg']};
+export const TagContainer = styled(View)<{ backColor: ColorType }>`
+  background-color: ${({ backColor }) => theme.color[backColor]};
   padding: 3px 6px;
   border-radius: 4px;
   align-self: flex-start;
--- src/components/common/CustomTag/index.tsx
+++ src/components/common/CustomTag/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { TagContainer } from './index.styles';
 import CustomText from '../CustomText';
+import { ColorType } from '../../../styles/theme';
+
+export type CustomTagVariant = 'default' | 'highlight';
 
 interface CustomTagProps {
   children: React.ReactNode;
+  variant?: CustomTagVariant;
 }
 
-const CustomTag = ({ children }: CustomTagProps) => {
+const CustomTag = ({ children, variant }: CustomTagProps) => {
+  const pointColorMap: Record<CustomTagVariant, ColorType> = {
+    default: 'gray/gray500',
+    highlight: 'main_color/yellow_p',
+  };
+  const backColorMap: Record<CustomTagVariant, ColorType> = {
+    default: 'gray/gray50',
+    highlight: 'sub_color/yellow/bg',
+  };
+
+  const pointColor = pointColorMap[variant ?? 'default'];
+  const backColor = backColorMap[variant ?? 'default'];
+
   return (
-    <TagContainer>
-      <CustomText size={10} weight="bold" lineHeight="sm" color="main_color/yellow_p">
+    <TagContainer backColor={backColor}>
+      <CustomText size={10} weight="bold" lineHeight="sm" color={pointColor}>
         {children}
       </CustomText>
     </TagContainer>
