5a84933e097f9816b27b8ee679b0231160b15904
cyLee1111 <flora021111@gmail.com>
2025-05-26 18:21:58 +0900

    refactor: remove erroneous navigation from interceptor and add sign-out logic

--- src/apis/interceptor.ts
+++ src/apis/interceptor.ts
@@ -1,7 +1,6 @@
-import axios, { AxiosResponse } from 'axios';
+import axios from 'axios';
 import { HTTP_HEADERS, HTTP_HEADERS_VALUES } from './constants';
-import { ParamListBase, useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import useAuthStore from '../store/useAuthStore';
 
 export const apiClient = axios.create({
   baseURL: `${process.env.API_URL}`,
@@ -12,46 +11,18 @@ export const apiClient = axios.create({
 
 apiClient.interceptors.response.use(
   (response) => response,
-  (error) => {
-    console.error('Error!!: ' + error);
-    const errorResponse = error.response?.data?.errorResponse;
-    const errorMessage = errorResponse?.message || error.message || 'An unknown error occurred.';
-    const errorStatus = errorResponse?.status || 'Unknown Status';
-    const errorCode = errorResponse?.code || error.code || 'Unknown Code';
-    console.error('errorMessage: ' + errorMessage);
-    console.error('errorStatus: ' + errorStatus);
-    console.error('errorCode: ' + errorCode);
-
+  async (error) => {
     if (axios.isAxiosError(error)) {
-      const errorResponse = error.response?.data?.errorResponse;
-
-      const errorMessage = errorResponse?.message || error.message || 'An unknown error occurred.';
-      const errorStatus = errorResponse?.status || 'Unknown Status';
-      const errorCode = errorResponse?.code || error.code || 'Unknown Code';
-      // const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
+      const errorCode = error.response?.data?.errorResponse?.code;
 
       if (errorCode === 'J4010') {
         console.log('Access Token 만료!!');
-        // navigation.navigate('Signin');
-        return Promise.resolve({
-          data: null,
-          status: 401,
-          statusText: 'Unauthorized',
-          headers: {},
-          config: error.config,
-        } as AxiosResponse<any>);
-      }
-
-      console.error(`Axios Error: ${errorMessage}
-        Status: ${errorStatus}
-        Code: ${errorCode}`);
-      console.error('Request Config:', error.config);
-      if (error.request) {
-        console.error('Request:', error.request);
+        // TODO: token test
+        await useAuthStore.getState().signout();
       }
     }
     return Promise.reject(error);
-  }
+  },
 );
 
 export default apiClient;
