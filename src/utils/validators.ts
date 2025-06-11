842adaa91bbf0c6bac97eef1a0956e5b041bd829
cyLee1111 <flora021111@gmail.com>
2025-06-11 14:54:42 +0900

    feat: add validators util

---
+++ src/utils/validators.ts
@@ -0,0 +1,5 @@
+export const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+export const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/;
+
+export const nicknameRegex = /^[\p{L}0-9]{2,8}$/u;
