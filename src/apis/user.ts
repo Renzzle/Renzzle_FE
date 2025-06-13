67e810078a54136ebe3b2e8a40ded40ab08140fe
cyLee1111 <flora021111@gmail.com>
2025-06-13 17:33:14 +0900

    feat: add MyPuzzles screen

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -15,10 +15,22 @@ export const getUser = async (authStore: string) => {
   }
 };
 
-export const updateLike = async (
-  authStore: string,
-  puzzleId: number,
-) => {
+export const getUserPuzzles = async (id: number | null, size: number) => {
+  const params: Record<string, any> = { size };
+  if (id !== null) {
+    params.id = id;
+  }
+
+  try {
+    const response = await apiClient.get('/api/user/puzzle', { params });
+
+    return response.data.response;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateLike = async (authStore: string, puzzleId: number) => {
   try {
     const response = await apiClient.post(
       '/api/user/like',
@@ -27,7 +39,7 @@ export const updateLike = async (
         headers: {
           [HTTP_HEADERS.AUTHORIZATION]: `Bearer ${authStore}`,
         },
-      }
+      },
     );
 
     console.log('like api:' + response.data.response);
@@ -38,15 +50,15 @@ export const updateLike = async (
   }
 };
 
-export const getLikePuzzle = async (
-  authStore: string,
-  size?: number,
-  id?: number,
-) => {
+export const getLikePuzzle = async (authStore: string, size?: number, id?: number) => {
   try {
     const params: Record<string, string | number> = {};
-    if (id !== undefined) {params.id = id;}
-    if (size !== undefined) {params.size = size;}
+    if (id !== undefined) {
+      params.id = id;
+    }
+    if (size !== undefined) {
+      params.size = size;
+    }
 
     const response = await apiClient.get('/api/user/like', {
       headers: {
---
+++ src/screens/MyPuzzles/index.styles.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+import styled from 'styled-components';
+import theme from '../../styles/theme';
+
+export const Container = styled(View)`
+  padding: 0 15px 15px;
+  background-color: ${theme.color['gray/grayBGDim']};
+  flex: 1;
+`;
