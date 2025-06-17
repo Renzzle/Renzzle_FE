a45cf35788811b796157b18727cd1a67eda27867
cyLee1111 <flora021111@gmail.com>
2025-06-17 10:44:43 +0900

    feat: add react-native-safe-area-context module

--- App.tsx
+++ App.tsx
@@ -28,6 +28,7 @@ import CommunityPuzzles from './src/screens/CommunityPuzzles/index.tsx';
 import CommunityPuzzleSolve from './src/screens/CommunityPuzzleSolve/index.tsx';
 import CreateCommunityPuzzle from './src/screens/CreateCommunityPuzzle/index.tsx';
 import RankedPuzzleSolve from './src/screens/RankedPuzzleSolve/index.tsx';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
@@ -42,76 +43,78 @@ function App(): React.JSX.Element | null {
   }
 
   return (
-    <NavigationContainer>
-      <AppWrapper>
-        <Stack.Navigator
-          screenOptions={{
-            header: renderCustomHeader,
-          }}>
-          {accessToken ? (
-            <>
-              <Stack.Screen name="Home" component={Home} options={{ title: 'common.appName' }} />
-              <Stack.Screen
-                name="MyPuzzles"
-                component={MyPuzzles}
-                options={{ title: 'common.myPuzzle' }}
-              />
-              <Stack.Screen
-                name="LikedPuzzles"
-                component={LikedPuzzles}
-                options={{ title: 'common.likes' }}
-              />
-              <Stack.Screen
-                name="Ranking"
-                component={Ranking}
-                options={{ title: 'common.ranking' }}
-              />
-              <Stack.Screen
-                name="TrainingPacks"
-                component={TrainingPacks}
-                options={{ title: 'home.trainingPuzzle' }}
-              />
-              <Stack.Screen
-                name="TrainingPuzzles"
-                component={TrainingPuzzles}
-                options={{ title: 'home.trainingPuzzle' }}
-              />
-              <Stack.Screen
-                name="TrainingPuzzleSolve"
-                component={TrainingPuzzleSolve}
-                options={{ title: 'home.trainingPuzzle' }}
-              />
-              <Stack.Screen
-                name="CommunityPuzzles"
-                component={CommunityPuzzles}
-                options={{ title: 'home.communityPuzzle' }}
-              />
-              <Stack.Screen
-                name="CommunityPuzzleSolve"
-                component={CommunityPuzzleSolve}
-                options={{ title: 'home.communityPuzzle' }}
-              />
-              <Stack.Screen
-                name="CreateCommunityPuzzle"
-                component={CreateCommunityPuzzle}
-                options={{ title: 'home.communityPuzzle' }}
-              />
-              <Stack.Screen
-                name="RankedPuzzleSolve"
-                component={RankedPuzzleSolve}
-                options={{ title: 'home.rankingPuzzle' }}
-              />
-            </>
-          ) : (
-            <>
-              <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false }} />
-              <Stack.Screen name="Signup" component={Signup} options={{ title: 'auth.signup' }} />
-            </>
-          )}
-        </Stack.Navigator>
-      </AppWrapper>
-      <Toast config={toastConfig} />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <AppWrapper>
+          <Stack.Navigator
+            screenOptions={{
+              header: renderCustomHeader,
+            }}>
+            {accessToken ? (
+              <>
+                <Stack.Screen name="Home" component={Home} options={{ title: 'common.appName' }} />
+                <Stack.Screen
+                  name="MyPuzzles"
+                  component={MyPuzzles}
+                  options={{ title: 'common.myPuzzle' }}
+                />
+                <Stack.Screen
+                  name="LikedPuzzles"
+                  component={LikedPuzzles}
+                  options={{ title: 'common.likes' }}
+                />
+                <Stack.Screen
+                  name="Ranking"
+                  component={Ranking}
+                  options={{ title: 'common.ranking' }}
+                />
+                <Stack.Screen
+                  name="TrainingPacks"
+                  component={TrainingPacks}
+                  options={{ title: 'home.trainingPuzzle' }}
+                />
+                <Stack.Screen
+                  name="TrainingPuzzles"
+                  component={TrainingPuzzles}
+                  options={{ title: 'home.trainingPuzzle' }}
+                />
+                <Stack.Screen
+                  name="TrainingPuzzleSolve"
+                  component={TrainingPuzzleSolve}
+                  options={{ title: 'home.trainingPuzzle' }}
+                />
+                <Stack.Screen
+                  name="CommunityPuzzles"
+                  component={CommunityPuzzles}
+                  options={{ title: 'home.communityPuzzle' }}
+                />
+                <Stack.Screen
+                  name="CommunityPuzzleSolve"
+                  component={CommunityPuzzleSolve}
+                  options={{ title: 'home.communityPuzzle' }}
+                />
+                <Stack.Screen
+                  name="CreateCommunityPuzzle"
+                  component={CreateCommunityPuzzle}
+                  options={{ title: 'home.communityPuzzle' }}
+                />
+                <Stack.Screen
+                  name="RankedPuzzleSolve"
+                  component={RankedPuzzleSolve}
+                  options={{ title: 'home.rankingPuzzle' }}
+                />
+              </>
+            ) : (
+              <>
+                <Stack.Screen name="Signin" component={Signin} options={{ headerShown: false }} />
+                <Stack.Screen name="Signup" component={Signup} options={{ title: 'auth.signup' }} />
+              </>
+            )}
+          </Stack.Navigator>
+        </AppWrapper>
+        <Toast config={toastConfig} />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
