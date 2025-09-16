81f8b6749cb735026322dc85fd2b221880e82ed1
cyLee1111 <flora021111@gmail.com>
2025-09-17 00:09:21 +0900

    feat: add RankingResultButton component and type
    refactor: rename

---
+++ src/components/features/RankingResultButton/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Container } from './index.styles';
+import { RankingResultTheme, ResultVariant } from '../../types/Ranking';
+import { CustomText, Icon } from '../../common';
+
+interface Props {
+  variant: ResultVariant;
+  text: string;
+  disabled?: boolean;
+  onPress: () => void;
+}
+
+const RankingResultButton = ({ variant, text, disabled = false, onPress }: Props) => {
+  const selectedVarient = RankingResultTheme[variant];
+
+  return (
+    <Container variant={variant} disabled={disabled} onPress={onPress}>
+      <Icon name={selectedVarient.iconName} size={24} color={selectedVarient.primary} />
+      <CustomText size={14} weight="bold" lineHeight="sm" color={selectedVarient.primary}>
+        #{text}
+      </CustomText>
+    </Container>
+  );
+};
+
+export default RankingResultButton;
--- src/components/types/Ranking/index.ts
+++ src/components/types/Ranking/index.ts
@@ -1,3 +1,6 @@
+import { IconName } from '../../../assets/icons';
+import { ColorType } from '../../../styles/theme';
+
 export interface RatingRanking {
   rank: number;
   nickname: string;
@@ -19,3 +22,29 @@ export interface BestRankingResponse {
   top100: BestRanking[];
   myPuzzlerRank: BestRanking;
 }
+
+export type ResultVariant = 'success' | 'error' | 'default';
+
+type ResultTheme = {
+  primary: ColorType;
+  secondary: ColorType;
+  iconName: IconName;
+};
+
+export const RankingResultTheme: Record<ResultVariant, ResultTheme> = {
+  success: {
+    primary: 'main_color/blue_p',
+    secondary: 'sub_color/indigo/bg',
+    iconName: 'CheckIcon',
+  },
+  error: {
+    primary: 'main_color/yellow_p',
+    secondary: 'sub_color/yellow/bg',
+    iconName: 'CloseIcon',
+  },
+  default: {
+    primary: 'gray/gray400',
+    secondary: 'gray/gray100',
+    iconName: 'CloseIcon',
+  },
+};
