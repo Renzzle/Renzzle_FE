709aaa14fb38793560db154416e76688acf1da68
cyLee1111 <flora021111@gmail.com>
2026-03-17 17:27:37 +0900

    refactor: replace nickname edit container with DismissKeyboardView

--- src/screens/Settings/ChangeNickname/index.styles.tsx
+++ src/screens/Settings/ChangeNickname/index.styles.tsx
@@ -1,8 +1,8 @@
-import { View } from 'react-native';
 import styled from 'styled-components';
 import theme from '../../../styles/theme';
+import DismissKeyboardView from '../../../components/common/DismissKeyboadView';
 
-export const Container = styled(View)`
+export const Container = styled(DismissKeyboardView)`
   background-color: ${theme.color['gray/grayBG']};
   flex: 1;
   position: relative;
