88a8a8b436e9611c7a9fa14b99c56bcca3e786a9
cyLee1111 <flora021111@gmail.com>
2026-06-16 16:13:15 +0900

    feat: add locales to puzzle review

--- src/components/features/ReviewButton/index.tsx
+++ src/components/features/ReviewButton/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { CustomText } from '../../common';
 import { ButtonContainer } from './index.styles';
+import { useTranslation } from 'react-i18next';
 
 interface ReviewButtonProps {
   onPress: () => void;
 }
 
 const ReviewButton = ({ onPress }: ReviewButtonProps) => {
+  const { t } = useTranslation();
   return (
     <ButtonContainer onPress={onPress}>
       <CustomText weight="bold" lineHeight="sm" color="main_color/blue_p">
-        검토하기
+        {t('puzzle.review')}
       </CustomText>
     </ButtonContainer>
   );
